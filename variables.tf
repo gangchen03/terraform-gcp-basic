f64e5a5738984db8d44d56182dcc84c66407b1b6
Gang Chen <gangcchen@google.com>
2022-02-20 09:45:00 -0600

    Upload from local workstation

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project" {}
+
+variable "credentials_file" {}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "cidrs" { default = [] }
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "machine_types" {
+  type = map(any)
+  default = {
+    dev  = "f1-micro"
+    test = "n1-highcpu-32"
+    prod = "n1-highcpu-32"
+  }
+}
